8982f86b64887561206336519159e7f9541eaed6
zulhijaya18 <32.zulhijaya@gmail.com>
2022-04-27 10:36:55 +0800

    update field names

--- config/database.js
+++ config/database.js
@@ -1,15 +1,3 @@
-// const path = require('path');
-
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'sqlite',
-//     connection: {
-//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', 'db/data.db')),
-//     },
-//     useNullAsDefault: true,
-//   },
-// });
-
 module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
