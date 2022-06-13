b29175571a790bd5166e1dc9cc0732a0cd483f95
zulhijaya18 <32.zulhijaya@gmail.com>
2022-06-13 20:39:07 +0800

    update

--- src/api/wilayah/controllers/wilayah.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  wilayah controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::wilayah.wilayah');
--- src/api/wilayah/routes/wilayah.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * wilayah router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::wilayah.wilayah');
