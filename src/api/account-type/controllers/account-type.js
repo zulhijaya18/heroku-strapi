f29a14cd9c21d79a2b3f98c5b2eb9efa9da46d4e
zulhijaya18 <32.zulhijaya@gmail.com>
2022-06-17 21:38:52 +0800

    update user

---
+++ src/api/account-type/controllers/account-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  account-type controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::account-type.account-type');
---
+++ src/api/account-type/routes/account-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * account-type router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::account-type.account-type');
