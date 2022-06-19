1d87c320036890d63635742c79c11738568160de
zulhijaya18 <32.zulhijaya@gmail.com>
2022-06-20 01:23:01 +0800

    update

--- config/database.js
+++ config/database.js
@@ -1,13 +1,24 @@
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'mysql',
+//     connection: {
+//       host: env('DATABASE_HOST', '103.55.39.44'),
+//       port: env.int('DATABASE_PORT', 3306),
+//       database: env('DATABASE_NAME', 'ortentiv_simarkas'),
+//       user: env('DATABASE_USERNAME', 'ortentiv_zulhijaya'),
+//       password: env('DATABASE_PASSWORD', 'Zulhijaya123'),
+//       ssl: env.bool('DATABASE_SSL', false),
+//     },
+//   },
+// });
+
 module.exports = ({ env }) => ({
   connection: {
-    client: 'mysql',
+    client: 'sqlite',
     connection: {
-      host: env('DATABASE_HOST', '103.55.39.44'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'ortentiv_simarkas'),
-      user: env('DATABASE_USERNAME', 'ortentiv_zulhijaya'),
-      password: env('DATABASE_PASSWORD', 'Zulhijaya123'),
-      ssl: env.bool('DATABASE_SSL', false),
+      filename: env('DATABASE_FILENAME', 'tmp/data.db'),
     },
+    useNullAsDefault: true,
+    debug: false,
   },
 });
