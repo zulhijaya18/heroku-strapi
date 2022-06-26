0c6dadf57bd51911f3121ccbe91f4b9bf13d01f9
zulhijaya18 <32.zulhijaya@gmail.com>
2022-06-26 17:30:06 +0800

    update

--- config/database.js
+++ config/database.js
@@ -1,24 +1,24 @@
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'mysql',
-//     connection: {
-//       host: env('DATABASE_HOST', '103.55.39.44'),
-//       port: env.int('DATABASE_PORT', 3306),
-//       database: env('DATABASE_NAME', 'ortentiv_simarkas'),
-//       user: env('DATABASE_USERNAME', 'ortentiv_zulhijaya'),
-//       password: env('DATABASE_PASSWORD', 'Zulhijaya123'),
-//       ssl: env.bool('DATABASE_SSL', false),
-//     },
-//   },
-// });
-
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'mysql',
     connection: {
-      filename: env('DATABASE_FILENAME', 'tmp/data.db'),
+      host: env('DATABASE_HOST', '103.55.39.44'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'ortentiv_simarkas'),
+      user: env('DATABASE_USERNAME', 'ortentiv_zulhijaya'),
+      password: env('DATABASE_PASSWORD', 'Zulhijaya123'),
+      ssl: env.bool('DATABASE_SSL', false),
     },
-    useNullAsDefault: true,
-    debug: false,
   },
 });
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: env('DATABASE_FILENAME', 'tmp/data.db'),
+//     },
+//     useNullAsDefault: true,
+//     debug: false,
+//   },
+// });
