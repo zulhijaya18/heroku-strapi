f614833b1709d3791fb98fddf74b895ab78436e7
zulhijaya18 <32.zulhijaya@gmail.com>
2022-07-20 23:10:37 +0800

    update

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,14 @@
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: false,
+      headers: '*',
+      origin: ['http://localhost:1337', '*']
+    }
+  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
